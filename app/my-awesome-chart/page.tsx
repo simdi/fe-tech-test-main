f3bdc0262022a21af909fc1ab9b7c4005f3ad02f
Chisimdi Ezeanieto <simdi4iverson@yahoo.co.uk>
2023-07-12 16:41:59 +0100

    Tried formatting the chart.

--- app/my-awesome-chart/page.tsx
+++ app/my-awesome-chart/page.tsx
@@ -19,14 +19,13 @@ import { GridContainer } from "@/components/grid-container";
 import { cn } from "@/lib/utils";
 
 type XDataPlot = {
-  x: string;
+  x?: string;
   y: number;
 }
 
 export default function AwesomeChart() {
   const [data, setData] = useState<Array<null> | Array<XDataPlot>>([]);
   const [data2013, setData2013] = useState<Array<null> | Array<XDataPlot>>([]);
-  const [labels, setLabels] = useState([]);
 
   useEffect(() => {
     const fetchData = async() => {
@@ -45,7 +44,7 @@ export default function AwesomeChart() {
             const [date, currency] = curr;
             const newDate = new Date(date);
             acc.push({
-              x: new Date(newDate).toDateString(),
+              // x: new Date(newDate).toDateString(),
               y: currency.USD
             });
             return acc;
@@ -53,7 +52,7 @@ export default function AwesomeChart() {
           const resData2013Reduced = Object.entries(resData2013.rates).reduce((acc: Array<XDataPlot>, curr: any) => {
             const [date, currency] = curr;
             acc.push({
-              x: new Date(new Date(date).setFullYear(2023)).toDateString(),
+              x: new Date(new Date(date).setFullYear(2023)).toLocaleString().split(',')[0],
               y: currency.USD
             });
             return acc;
@@ -70,6 +69,7 @@ export default function AwesomeChart() {
     fetchData();
   }, []);
 
+
   return (
     <GridContainer>
       <Title>My Awesome Chart</Title>
@@ -78,17 +78,21 @@ export default function AwesomeChart() {
       <Card className={cn("w-full bg-white")}>
           <CardContent>
             <VictoryChart
-            minDomain={{ y: 1.1 }}
-            maxDomain={{ y: 1.7 }}
-            theme={VictoryTheme.material}
-            style={{
-              background: { fill: "white" }
-            }}
-            height={200}
-          >
+              theme={VictoryTheme.material}
+              style={{
+                background: { fill: "white" }
+              }}
+              domain={{
+                // x: [new Date(2023, 1, 1), new Date(2023, 2, 1)],
+                y: [1.1, 1.7]
+              }}
+              scale={{ x:"time", y:"linear" }}
+              height={200}
+              width={700}
+            >
             <VictoryLabel text="My Awesome Chart" x={70} y={10} textAnchor="middle"/>
             <VictoryLegend
-              x={190}
+              x={500}
               y={25}
               width={200}
               orientation="horizontal"
@@ -98,19 +102,19 @@ export default function AwesomeChart() {
                 labels: { fontSize: 5},
               }}
               data={[
-                { name: "Jan 2013: GBP to USD", symbol: { fill: "grey" } },
-                { name: "Jan 2023: GBP to USD", symbol: { fill: "blue" } },
+                { name: "Jan 2013: GBP to USD", symbol: { fill: "#d3d3d3" } },
+                { name: "Jan 2023: GBP to USD", symbol: { fill: "#7373fc" } },
               ]}
             />
             <VictoryLine
               style={{
-                data: { strokeDasharray: "grey" },
+                data: { stroke: "#d3d3d3", strokeDasharray: 2, strokeWidth: 1 },
               }}
               data={data2013}
             />
             <VictoryLine
               style={{
-                data: { stroke: "blue" },
+                data: { stroke: "#7373fc", strokeWidth: 1 },
               }}
               data={data}
             />
@@ -126,11 +130,11 @@ export default function AwesomeChart() {
               style={{
                 axis: {stroke: "#756f6a"},
                 axisLabel: {fontSize: 7, padding: 20},
-                grid: { stroke: ({ tick }) => tick },
-                ticks: {stroke: "grey", size: 5},
+                grid: { stroke: (tick: any) => tick },
+                ticks: {stroke: "grey", size: 3 },
                 tickLabels: {fontSize: 5, padding: 5}
               }}
-              tickValues={[2.11, 3.9, 6.1, 8.05]}
+              tickFormat={(t) => t}
             />
           </VictoryChart>
           </CardContent>
